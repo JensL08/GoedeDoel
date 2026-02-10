a819a0956f0b027c81945eedd40964b44de8ed17
jensl <jens.leyman@vdabcampus.be>
2026-02-10 10:08:20 +0100

    foute test

--- src/main/java/GoedeDoel.java
+++ src/main/java/GoedeDoel.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
+import java.math.BigDecimal;
 
 public class GoedeDoel {
+    private final String naam;
+    private BigDecimal opbrengst;
+    public GoedeDoel(String naam) {
+        this.naam = naam;
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public BigDecimal getOpbrengst() {
+        return opbrengst;
+    }
 }
--- src/test/java/GoedeDoelTest.java
+++ src/test/java/GoedeDoelTest.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-public class GoedeDoelTest {
+import static org.assertj.core.api.Assertions.assertThat;
+class GoedeDoelTest {
+    private static final String NAAM = "CliniClowns";
+    private GoedeDoel doel;
+    @BeforeEach
+    void beforeEach() {
+        doel = new GoedeDoel(NAAM);
+    }
+    @Test
+    void getNaam() {
+        assertThat(doel.getNaam()).isEqualTo(NAAM);
+    }
+    @Test
+    void eenNieuwDoelHeeftNogGeenOpbrengst() {
+        assertThat(doel.getOpbrengst()).isZero();
+    }
+
 }
